3e2ecb5e6ba7c2bf344f3241e8de8c95a82ea911
sontivr <sontivr@yahoo.com>
2018-04-24 22:33:30 -0400

    Add files via upload

---
+++ big-data-2/json/rt_plot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+#import matplotlib
+#matplotlib.use("agg")
+
+import matplotlib.pyplot as plt
+import time
+import random
+from collections import deque
+import numpy as np
+
+
+# simulates input from serial port
+def random_gen():
+    while True:
+        val = random.randint(1,10)
+        yield val
+        time.sleep(0.1)
+
+
+a1 = deque([0]*100)
+ax = plt.axes(xlim=(0, 20), ylim=(0, 10))
+d = random_gen()
+
+line, = plt.plot(a1)
+plt.ion()
+plt.ylim([0,10])
+plt.show()
+
+for i in range(0,20):
+    a1.appendleft(next(d))
+    datatoplot = a1.pop()
+    line.set_ydata(a1)
+    plt.draw()
+    print a1[0]
+    i += 1
+    time.sleep(0.1)
+    plt.pause(0.0001)                       #add this it will be OK.
---
+++ big-data-2/sensor/a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4]
+y = [4,3,2,1]
+
+plt.plot(x, y)
+plt.show()
